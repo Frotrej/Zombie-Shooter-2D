747c956d4ae822a92438c7dc886b243b46abfe2c
Frotrej <f.faszczowy@gmail.com>
2022-07-28 22:50:50 +0200

    praca nad strzelaniem
    Strzały poprawnie wykrywają i odejmują życie przeciwnikom.
    Usunięto feedback trafień raycastu

--- Game Zombie Shooter/Assets/Script/PistolShooting.cs
+++ Game Zombie Shooter/Assets/Script/PistolShooting.cs
@@ -5,29 +5,55 @@ using UnityEngine;
 public class PistolShooting : MonoBehaviour
 {
     [SerializeField]
-    int weaponDMG = 3;
-    int layerMask = 1 << 8;
+    int weaponDMG = 3; // zmienna tymczasowa nalezy stworzyc plik z lista obrazen broni
+    [SerializeField]
+    float weaponCoolDown = 0.4f;
+    float timeToNextFire = 0;
+
+    int layerMask = 1 << 8; //ustawienie zmiennej na warstwe Player
 
     private void Start()
     {
-        layerMask = ~layerMask;
+        layerMask = ~layerMask; //inwersja zmiennej, aby raycast ignorowa³ tylko warstwe gracza
     }
 
     void FixedUpdate()
     {
+        
+        if (Time.time > timeToNextFire)
+        {
+            timeToNextFire = Time.time + weaponCoolDown;    //dodanie opoznienia broni do czasu natepnego strzalu
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down), 10f, layerMask);
-         //nie dziala no i tyle
-        if (hit)
-        {
-            Debug.DrawLine(transform.position, hit.point, Color.red);
-            print($"Trafienie: {hit.collider.name}");
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down), 10f, layerMask);
+            if (hit)
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red);
+                    //print($"Trafienie: {hit.collider.name}");     //pokaz nazwe trafionego obiektu
+
+                    CheckTargetLayer(hit);//
+                }
+                else
+                {
+                    //iluzja rysowania lini prostej przez mnozenie drugiego punktu *1000
+                    Debug.DrawLine(transform.position, transform.TransformDirection(Vector2.down) * 1000, Color.magenta);
+
+                    //print("Brak trafienia");
+                }
         }
-        else
+    }
+
+    void CheckTargetLayer(RaycastHit2D targetInfo)
+    {
+        if (targetInfo.collider.gameObject.CompareTag("EntityEnemy"))
         {
-            Debug.DrawLine(transform.position, transform.TransformDirection(Vector2.down) * 1000, Color.green);
-            print("Brak trafienia");
+            //print("to przeciwnik");
+            targetInfo.collider.gameObject.SendMessage("DealDMG", weaponDMG);
         }
-        print(layerMask);
+    }
+
+    void empty()
+    {
+        float time = Time.time;
     }
 }
